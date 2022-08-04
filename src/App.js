832814fd447bb7502f629257d2b9e82afcd16ece
JarvAmrit <amritkr1994@gmail.com>
2022-08-04 06:59:16 +0530

    Initial Commit Test7

--- src/App.js
+++ src/App.js
@@ -250,7 +250,7 @@ function App() {
               <div className="card shadow mb-4">
                 <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 className="m-0 font-weight-bold text-primary">
-                    Revenue Sources
+                    Distribution
                   </h6>
                   <div className="dropdown no-arrow">
                     <a
